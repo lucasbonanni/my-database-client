8d56957129f80620136b9812fd0e013fba3c0d61
Lucas Bonanni <bonanni.lucas.esteban@gmail.com>
2022-07-16 20:55:32 -0300

    Refactorizo el toolbar y paso la logica al main controller

--- src/presentacion/MainController.java
+++ src/presentacion/MainController.java
@@ -49,7 +49,7 @@ public class MainController {
         buildTreeView();
 
         this.mainMenuBar.build();
-        this.toolBar.build();
+        this.buildMainToolbar();
         this.genericService.addListener(this.gridController);
         buildMainFrame();
     }
@@ -58,7 +58,7 @@ public class MainController {
         JButton btnExecute = this.toolBar.getBtnExecute();
         btnExecute.addMouseListener(new ExecuteEventListener(this.queryEditorPane,this.genericService));
 
-        toolBar.addBtnClearTextAction((e -> {
+        toolBar.getBtnClearText().addActionListener((e -> {
             this.queryEditorPane.clearText();
         }));
         JMenuItem connectionManager = this.mainMenuBar.getConnectionManager();
@@ -108,11 +108,54 @@ public class MainController {
         this.treeViewPane.build();
     }
 
+    public void buildMainToolbar(){
+
+        try {
+            connectionService.loadConnectionsData();
+        } catch (ServiceException ex) {
+            JOptionPane.showMessageDialog(this.toolBar, ex.getMessage(), "Error al intentar leer el archivo", JOptionPane.ERROR_MESSAGE);
+        }
+        setComboData();
+        connectionService.addConnectionsChangedListener((e->setComboData()));
+        JPanel panel1 = new JPanel();
+        panel1.add(this.toolBar.getConnectionsCombo());
+        panel1.add(this.toolBar.getBtnConnect()  );
+        panel1.add(this.toolBar.getBtnDisconnect() );
+        panel1.add(this.toolBar.getConnectionStatus() );
+        this.toolBar.add(panel1);
+        JPanel panel2 = new JPanel();
+        panel2.add(this.toolBar.getBtnExecute());
+        this.toolBar.add(panel2);
+        JPanel panel3 = new JPanel();
+        panel3.add(this.toolBar.getBtnClearText());
+        this.toolBar.add(panel3);
+        this.toolBar.getConnectionsCombo().addItemListener(e -> connectionService.setSelectedConnection((dao.ConnectionData)toolBar.getConnectionsCombo().getSelectedItem()));
+        this.toolBar.getBtnConnect().addActionListener((e -> {
+            try {
+                connectionService.connect();
+                this.toolBar.getConnectionStatus().setText("Conectado");
+            }
+            catch (ServiceException ex){
+                JOptionPane.showMessageDialog(this.toolBar, ex.getMessage(), "Error al establecer la conexión", JOptionPane.ERROR_MESSAGE);
+            }
+        }));
+        this.toolBar.getBtnDisconnect().addActionListener((e -> {
+            try {
+                connectionService.disconnect();
+                this.toolBar.getConnectionStatus().setText("Desconectado");
+            }
+            catch (ServiceException ex){
+                JOptionPane.showMessageDialog(this.toolBar, "Ocurrió un problema al desconectar", "Error al desconectar", JOptionPane.ERROR_MESSAGE);
+            }
+        }));
+        connectionService.setSelectedConnection((dao.ConnectionData) this.toolBar.getConnectionsCombo().getSelectedItem());
+
+    }
+
     public void showMainFrame()
     {
         this.mainFrame.showFrame();
     }
-
 
 
     private void getObjectsTree(DefaultMutableTreeNode root, ArrayList<String> schemas) {
@@ -131,5 +174,10 @@ public class MainController {
                 views.add(new DefaultMutableTreeNode(nameParts[1]));
             }
         }
+    }
+
+    private void setComboData(){
+        DefaultComboBoxModel connections = new DefaultComboBoxModel<>(((ArrayList<dao.ConnectionData>) connectionService.getConnections().clone()).toArray()) ;
+        this.toolBar.getConnectionsCombo().setModel(connections);
     }
 }
--- src/presentacion/MainToolBar.java
+++ src/presentacion/MainToolBar.java
@@ -1,24 +1,38 @@
 package presentacion;
 
 import dao.ConnectionData;
-import service.ServiceException;
-import service.ConnectionService;
 
 import javax.swing.*;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.util.ArrayList;
 
-public class MainToolBar extends JToolBar implements ItemListener {
+public class MainToolBar extends JToolBar {
 
-    private final ConnectionService connectionService;
     JButton btnExecute;
-    JButton btnClearText;
-    JButton btnConnect;
-    JButton btnDisconnect;
-    JComboBox<ConnectionData> connectionsCombo;
-    JLabel connectionStatus;
+
+    public JButton getBtnClearText() {
+        return btnClearText;
+    }
+
+    public JButton getBtnConnect() {
+        return btnConnect;
+    }
+
+    public JButton getBtnDisconnect() {
+        return btnDisconnect;
+    }
+
+    public JComboBox<ConnectionData> getConnectionsCombo() {
+        return connectionsCombo;
+    }
+
+    public JLabel getConnectionStatus() {
+        return connectionStatus;
+    }
+
+    private JButton btnClearText;
+    private JButton btnConnect;
+    private JButton btnDisconnect;
+    private JComboBox<ConnectionData> connectionsCombo;
+    private JLabel connectionStatus;
     public JButton getBtnExecute() {
         return btnExecute;
     }
@@ -32,64 +46,7 @@ public class MainToolBar extends JToolBar implements ItemListener {
         btnDisconnect = new JButton("Desconectar");
         btnConnect = new JButton("Conectar");
         connectionsCombo = new JComboBox<>();
-        connectionService = ConnectionService.getInstance();
         connectionStatus = new JLabel("Desconectado");
     }
 
-    public void build() {
-        try {
-            connectionService.loadConnectionsData();
-        } catch (ServiceException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Error al intentar leer el archivo", JOptionPane.ERROR_MESSAGE);
-        }
-        setComboData();
-        connectionService.addConnectionsChangedListener((e->setComboData()));
-        JPanel panel1 = new JPanel();
-        panel1.add(connectionsCombo);
-        panel1.add(btnConnect);
-        panel1.add(btnDisconnect);
-        panel1.add(connectionStatus);
-        this.add(panel1);
-        JPanel panel2 = new JPanel();
-        panel2.add(btnExecute);
-        this.add(panel2);
-        JPanel panel3 = new JPanel();
-        panel3.add(btnClearText);
-        this.add(panel3);
-        connectionsCombo.addItemListener(this);
-        btnConnect.addActionListener((e -> {
-            try {
-                connectionService.connect();
-                this.connectionStatus.setText("Conectado");
-            }
-            catch (ServiceException ex){
-                JOptionPane.showMessageDialog(this, ex.getMessage(), "Error al establecer la conexión", JOptionPane.ERROR_MESSAGE);
-            }
-        }));
-        btnDisconnect.addActionListener((e -> {
-            try {
-                connectionService.disconnect();
-                connectionStatus.setText("Desconectado");
-            }
-            catch (ServiceException ex){
-                JOptionPane.showMessageDialog(this, "Ocurrió un problema al desconectar", "Error al desconectar", JOptionPane.ERROR_MESSAGE);
-            }
-        }));
-        connectionService.setSelectedConnection((ConnectionData) this.connectionsCombo.getSelectedItem());
-    }
-
-    private void setComboData(){
-        DefaultComboBoxModel connections = new DefaultComboBoxModel<>(((ArrayList<ConnectionData>) connectionService.getConnections().clone()).toArray()) ;
-        connectionsCombo.setModel(connections);
-    }
-
-    public void addBtnClearTextAction(ActionListener l){
-        btnClearText.addActionListener(l);
-    }
-
-    @Override
-    public void itemStateChanged(ItemEvent e) {
-       this.connectionService.setSelectedConnection((ConnectionData)this.connectionsCombo.getSelectedItem());
-
-    }
 }
